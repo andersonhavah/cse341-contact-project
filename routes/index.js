5ea3b8fe6c0a12ab6491378801d22e4e3bfb70c4
andersonhavah <havahanderson@gmail.com>
2026-01-16 16:04:31 +0000

    Added and made the app use the users route.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const usersRoute = require('./usersRoute');
 
 router.get('/', (req, res) => {
   res.send('Welcome to the Contact Management API');
 });
+
+router.use('/users', usersRoute);
 
 module.exports = router;
