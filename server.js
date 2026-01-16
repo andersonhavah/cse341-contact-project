8ce28333ed28498321d49a210fe5f933ebad7483
andersonhavah <havahanderson@gmail.com>
2026-01-16 16:00:45 +0000

    imported from the index database file for the data folder.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongodb =  require('./data/database');
 const app = express();
 const routes = require('./routes');
 
@@ -6,6 +7,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use('/', routes);
 
+mongodb.initDb((err) => {
+  if (err) {
+    console.log('Unable to connect to database', err);
+    process.exit(1);
+  } else {
+    console.log('Database connection established.');
+  } 
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Database is listening and node running on port ${PORT}.`);
 });
