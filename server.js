91ba784b9a6c04e4480440c6a1d8b86226f94b10
andersonhavah <havahanderson@gmail.com>
2026-01-24 21:37:40 +0000

    Added body parser

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongodb =  require('./data/database');
 const app = express();
 const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/', routes);
+app.use(bodyParser.json());
 
 mongodb.initDb((err) => {
   if (err) {
