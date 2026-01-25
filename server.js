bb22869ad01fba4cadbcc89f8de0240cada46fa7
andersonhavah <havahanderson@gmail.com>
2026-01-25 16:42:44 +0000

    Set the headers and make the app use them.

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+})
+
 app.use('/', routes);
 
 mongodb.initDb((err) => {
