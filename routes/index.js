2fc0a5f84b5422d8d32c0d505dd907d490d6030b
andersonhavah <havahanderson@gmail.com>
2026-01-25 16:41:34 +0000

    Added route to swaggerRoute and added tags to the '/' route.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const usersRoute = require('./usersRoute');
 
+router.use('/', require('./swaggerRoute'));
+
 router.get('/', (req, res) => {
+  //#swagger-tags=['Welcome to the Contact Management API']
   res.send('Welcome to the Contact Management API');
 });
 
